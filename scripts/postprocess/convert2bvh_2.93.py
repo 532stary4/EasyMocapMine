65f7c852a029f7098ad0a6fbc22ea422ea23561f
532stary4 <61381495+532stary4@users.noreply.github.com>
2021-10-16 22:34:11 +0100

    Update convert2bvh_2.93.py

--- scripts/postprocess/convert2bvh_2.93.py
+++ scripts/postprocess/convert2bvh_2.93.py
@@ -23,34 +23,53 @@ part_match = {'root': 'root', 'bone_00': 'Pelvis', 'bone_01': 'L_Hip', 'bone_02'
               'bone_15': 'Head', 'bone_16': 'L_Shoulder', 'bone_17': 'R_Shoulder', 'bone_18': 'L_Elbow',
               'bone_19': 'R_Elbow', 'bone_20': 'L_Wrist', 'bone_21': 'R_Wrist', 'bone_22': 'L_Hand', 'bone_23': 'R_Hand'}
 
+'''smplx_joint_names = {'root': 'root', 'bone_00': 'pelvis', 'bone_01': 'left_hip','right_hip',
+                     'spine1','left_knee','right_knee',
+                     'spine2','left_ankle','right_ankle',
+                     'spine3', 'left_foot','right_foot','neck','left_collar','right_collar','head','left_shoulder','right_shoulder','left_elbow', 'right_elbow','left_wrist','right_wrist',
+    'jaw','left_eye_smplhf','right_eye_smplhf','left_index1','left_index2','left_index3','left_middle1','left_middle2','left_middle3','left_pinky1','left_pinky2','left_pinky3','left_ring1','left_ring2','left_ring3','left_thumb1','left_thumb2','left_thumb3','right_index1','right_index2','right_index3','right_middle1','right_middle2','right_middle3','right_pinky1','right_pinky2','right_pinky3','right_ring1','right_ring2','right_ring3','right_thumb1','right_thumb2','right_thumb3'
+}'''
+
 
 def deg2rad(angle):
     return -np.pi * (angle + 90) / 180.
 
 
 def init_scene(params):
+    gender = params['gender']
     if (params['smplx']):
-        pass
-    else:
-        gender = params['gender']
+        try:
+            # loads an smplx model using the addon
+            bpy.data.window_managers["WinMan"].smplx_tool.smplx_gender = gender
+            bpy.ops.scene.smplx_add_gender()
 
+            obj_name = 'SMPLX-mesh-' + gender
+            arm_obj_name = 'SMPLX-' + gender
+
+        except AttributeError as e:
+            print('')
+            print(e)
+            print('Install and enable smplx addon for blender')
+            print('')
+    else:
         # load fbx model
-        bpy.ops.import_scene.fbx(filepath=join(params['smpl_data_folder'], 'basicModel_%s_lbs_10_207_0_v1.0.2.fbx' % gender[0]), axis_forward='-Y', axis_up='-Z', global_scale=100)
+        bpy.ops.import_scene.fbx(filepath=join(params['smpl_data_folder'], 'basicModel_%s_lbs_10_207_0_v1.0.2.fbx' % gender[0]), global_scale=100)
         
         obj_name = '%s_avg' % gender[0]
-        obj = bpy.data.objects[obj_name]
+        arm_obj_name = 'Armature'
 
-        # delete the default stuff
-        bpy.ops.object.select_all(action='DESELECT')
-        bpy.data.objects['Cube'].select_set(True)
-        bpy.data.objects['Camera'].select_set(True)
-        bpy.data.objects['Light'].select_set(True)
-        bpy.ops.object.delete()
+    # delete the default stuff
+    bpy.ops.object.select_all(action='DESELECT')
+    bpy.data.objects['Cube'].select_set(True)
+    bpy.data.objects['Camera'].select_set(True)
+    bpy.data.objects['Light'].select_set(True)
+    bpy.ops.object.delete()
 
-        # clear existing animation data
-        obj.data.shape_keys.animation_data_clear()
-        arm_obj = bpy.data.objects['Armature']
-        arm_obj.animation_data_clear()
+    # clear existing animation data
+    obj = bpy.data.objects[obj_name]
+    obj.data.shape_keys.animation_data_clear()
+    arm_obj = bpy.data.objects[arm_obj_name]
+    arm_obj.animation_data_clear()
 
     return (obj, obj_name, arm_obj)
 
@@ -64,11 +83,12 @@ def Rodrigues(rotvec):
                       [-r[1], r[0], 0]])
     return(cost*np.eye(3) + (1-cost)*r.dot(r.T) + np.sin(theta)*mat)
 
+
 def rodrigues2bshapes(pose):
+    print(len(pose))
     rod_rots = np.asarray(pose).reshape(24, 3)
     mat_rots = [Rodrigues(rod_rot) for rod_rot in rod_rots]
-    bshapes = np.concatenate([(mat_rot - np.eye(3)).ravel()
-                              for mat_rot in mat_rots[1:]])
+    bshapes = np.concatenate([(mat_rot - np.eye(3)).ravel() for mat_rot in mat_rots[1:]])
     return(mat_rots, bshapes)
 
 # apply trans pose and shape to character
@@ -77,18 +97,21 @@ def apply_trans_pose_shape(trans, pose, shape, obj, arm_obj, obj_name, frame):
     mrots, bsh = rodrigues2bshapes(pose)
 
     # set the location of the first bone to the translation parameter
-    arm_obj.pose.bones[obj_name+'_Pelvis'].location = trans
-    arm_obj.pose.bones[obj_name+'_root'].location = trans
-    arm_obj.pose.bones[obj_name +'_root'].keyframe_insert('location', frame=frame)
+    #arm_obj.pose.bones['pelvis'].location = trans
+    #arm_obj.pose.bones['root'].location = trans
+    #arm_obj.pose.bones['root'].keyframe_insert('location', frame=frame)
 
     # set the pose of each bone to the quaternion specified by pose
     for ibone, mrot in enumerate(mrots):
+        print(ibone, mrot)
+        continue
         bone = arm_obj.pose.bones[obj_name+'_'+part_match['bone_%02d' % ibone]]
         bone.rotation_quaternion = Matrix(mrot).to_quaternion()
         if frame is not None:
             bone.keyframe_insert('rotation_quaternion', frame=frame)
             bone.keyframe_insert('location', frame=frame)
 
+    quit()
     # apply pose blendshapes
     for ibshape, bshape in enumerate(bsh):
         obj.data.shape_keys.key_blocks['Pose%03d' % ibshape].value = bshape
@@ -139,7 +162,7 @@ def merge_params(param_list):
 def load_motions(datapath):
     from glob import glob
     filenames = sorted(glob(join(datapath, '*.json')))
-    print(len(filenames))
+    print('Frames ', len(filenames))
     motions = {}
     for filename in filenames:
         infos = read_smpl(filename)
@@ -158,33 +181,26 @@ def load_motions(datapath):
 def main(params):
     obj, obj_name, arm_obj = init_scene(params)
 
-    bpy.ops.object.select_all(action='DESELECT')
-    obj.select_set(True)
-    bpy.context.view_layer.objects.active = obj
-
     # unblocking both the pose and the blendshape limits
     for k in obj.data.shape_keys.key_blocks.keys():
-        bpy.data.shape_keys["Key"].key_blocks[k].slider_min = -10
-        bpy.data.shape_keys["Key"].key_blocks[k].slider_max = 10
+        bpy.data.shape_keys[0].key_blocks[k].slider_min = -10
+        bpy.data.shape_keys[0].key_blocks[k].slider_max = 10
 
-    bpy.context.view_layer.objects.active = arm_obj
     motions = load_motions(params['path'])
-
     for pid, data in motions.items():
 
-        # animation
         arm_obj.animation_data_clear()
 
         # load smpl params:
         nFrames = data['poses'].shape[0]
         for frame in range(nFrames):
-            print(frame)
             trans = data['Th'][frame]
             shape = data['shapes'][0]
             pose = data['poses'][frame]
-            apply_trans_pose_shape(Vector(trans), pose, shape, obj,
-                                arm_obj, obj_name, frame)
+
+            apply_trans_pose_shape(Vector(trans), pose, shape, obj, arm_obj, obj_name, frame)
             bpy.context.view_layer.update()
+
         bpy.ops.export_anim.bvh(filepath=join(params['out'], '{}.bvh'.format(pid)), frame_start=0, frame_end=nFrames-1)
     return 0
 
@@ -221,3 +237,4 @@ if __name__ == '__main__':
         # Only exit to OS when we are not running in Blender GUI
         if bpy.app.background:
             sys.exit(exit_status)
+
